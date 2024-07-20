f23e3acd840f7db6909be3bdcaaba177ba5afa9d
NiamhFerns <Niamh.K.Ferns@gmail.com>
2024-07-21 09:37:43 +1200

    Can now handle loose chapters and cleanly uses hidden temp files during build.

--- main.py
+++ main.py
@@ -10,48 +10,48 @@ import sys
 import shutil
 from zipfile import ZipFile
 
-progress_x = 0
-title = "MISSING TITLE"
+CURRENT_PROGRESS = 0
+TITLE = ""
+VOLUME_PATTERN = re.compile(".*(vol|v|vo|volume)\\d+.*\\.cbz", re.IGNORECASE)
+CHAPTER_PATTERN = re.compile("Vol. [1-9][0-9]* Ch. [1-9][0-9]*", re.IGNORECASE)
+PAGE_PATTERN = re.compile(".+(.jpeg|.jpg|.png)", re.IGNORECASE)
+CBZ_PATTERN = re.compile(".+\\.cbz", re.IGNORECASE)
 
 
 def start_progress(title):
-    global progress_x
+    global CURRENT_PROGRESS
     sys.stdout.write(title + " [" + "-" * 40 + "]" + chr(8) * 41)
     sys.stdout.flush()
-    progress_x = 0
+    CURRENT_PROGRESS = 0
 
 
-def progress(x):
-    global progress_x
+def increment_progress(x):
+    global CURRENT_PROGRESS
     x = int(x * 40 // 100)
-    sys.stdout.write("#" * (x - progress_x))
+    sys.stdout.write("#" * (x - CURRENT_PROGRESS))
     sys.stdout.flush()
-    progress_x = x
+    CURRENT_PROGRESS = x
 
 
 def end_progress():
-    sys.stdout.write("#" * (40 - progress_x) + "]\n")
+    sys.stdout.write("#" * (40 - CURRENT_PROGRESS) + "]\n")
     sys.stdout.flush()
 
 
 def build_chapters(base_directory, volume, volume_path):
     """
-    Creates the relevant loose folder directories for all Vol. n Ch. m entries in a volume format loose folder where "n"
-    is the volume number and "m" is the chapter number. These are then used to rename and rebuild cbzs in Vol. n Ch. m
-    format.
+    Builds chapters within a volume's directory into the relevant Vol. n Ch. m.cbz files.
     """
-    chapter_pattern = re.compile("Vol. [1-9][0-9]* Ch. [1-9][0-9]*")
-
     prepare_loose_chapters(volume, volume_path)
 
     chapters = [
-        path for path in os.listdir(volume_path) if re.match(chapter_pattern, path)
+        path for path in os.listdir(volume_path) if re.match(CHAPTER_PATTERN, path)
     ]
     for chapter in chapters:
         chapter_path = f"{volume_path}/{chapter}"
         print(f"[COMPRESSING] {chapter}")
         shutil.make_archive(chapter_path, "zip", chapter_path)
-        shutil.move(f"{chapter_path}.zip", f"{base_directory}/{chapter}.cbz")
+        shutil.move(f"{chapter_path}.zip", f"{base_directory}/_output/{chapter}.cbz")
     print(f"[CLEANING UP] Deleting {volume_path}")
     shutil.rmtree(volume_path)
 
@@ -61,58 +61,73 @@ def prepare_loose_chapters(volume, volume_path):
     Takes all loose pages in an unprepared volume directory and moves them to the appropriate Vol. n Ch. m folder where
     n is the volume number and m is the chapter number. Renames pages to page number format nnn.extn or nnn-nnn.extn.
     """
-    pages = glob.glob(f"{volume_path}/*.png")
-    pages += glob.glob(f"{volume_path}/*.jpg")
-    pages += glob.glob(f"{volume_path}/*.jpeg")
 
-    start_progress(f"[BUILDING] {volume_path[volume_path.rfind(title):]}")
-    for i in range(0, len(pages)):
-        chapter = re.search(r"c[0-9]{3}", pages[i])
+    pages = [path for path in os.listdir(volume_path) if re.match(PAGE_PATTERN, path)]
+
+    start_progress(f"[BUILDING] {volume_path[volume_path.rfind(TITLE):]}")
+    for i, page in enumerate(pages):
+        chapter = re.search(r"c[0-9]{3}", page)
         chapter = int(chapter.group(0)[1:])
         chapter_path = f"{volume_path}/Vol. {volume} Ch. {chapter}"
 
-        match = re.search(r"p[0-9]{3}-p[0-9]{3}", pages[i]) or re.search(
-            r"p[0-9]{3}", pages[i]
+        match = re.search(r"p[0-9]{3}-p[0-9]{3}", page) or re.search(
+            r"p[0-9]{3}", page
         )
-        page_name = f"{match.group(0).replace('p', '')}{pages[i][pages[i].rfind('.'):]}"
+        page_name = f"{match.group(0).replace('p', '')}{page[page.rfind('.'):]}"
         if match is None:
             raise Exception(
                 "You have an invalid file name in your path, please fix it first."
             )
         os.makedirs(os.path.dirname(f"{chapter_path}/{page_name}"), exist_ok=True)
-        shutil.move(pages[i], chapter_path + f"/{page_name}")
-        progress(int(i / len(pages) * 100))
+        shutil.move(f"{volume_path}/{page}", f"{chapter_path}/{page_name}")
+        increment_progress(int(i / len(pages) * 100))
     end_progress()
 
 
 def main():
-    global title
+    global TITLE
     base_directory = os.getcwd()
-    title = base_directory[base_directory.rfind("/") + 1 :]
+    # shutil.rmtree(f"{base_directory}/_output")
+    # os.remove(f"{base_directory}/.tmp.zip")
+    TITLE = base_directory[base_directory.rfind("/") + 1 :]
     if len(sys.argv) > 2:
         base_directory = sys.argv[1]
+    os.mkdir(f"{base_directory}/_output")
 
     # volume_paths in this case refers to the original volume.cbz files
-    volume_paths = glob.glob(f"{base_directory}/*.cbz")
-    for volume_path in volume_paths:
-        if volume_path.rfind(".cbz") == -1:
-            continue
+    volume_paths = [path for path in os.listdir(base_directory) if re.match(VOLUME_PATTERN, path)]
+    volumeless_chapter_paths = [path for path in os.listdir(base_directory) if not re.match(VOLUME_PATTERN, path)]
+    volumeless_chapter_paths = list(filter(lambda x: re.match(CBZ_PATTERN, x), volumeless_chapter_paths))
 
+    start_progress(f"[RENAMING] Renaming loose chapters...")
+    # Rename loose chapters that don't have a volume yet.
+    for i, chapter in enumerate(volumeless_chapter_paths):
+        chapter_number = re.search("[1-9]{1}\\d*", chapter)
+        if chapter_number and chapter_number.group(0) is None:
+            continue
+        chapter_number = chapter_number.group(0)
+
+        shutil.copy2(f"{base_directory}/{chapter}", f"{base_directory}/_output/Ch. {chapter_number}.cbz")
+        increment_progress(int(i / len(volumeless_chapter_paths) * 100))
+    end_progress()
+
+    # Handles volumes that need to be extracted and separated into chapters.
+    for volume_path in volume_paths:
         # We want to copy those to a tmp zip file (to leave originals intact) then extract and cleanup the unneeded volume.zip file.
         # Extracted loose files are just named with the number of the volume.
-        shutil.copy(volume_path, "tmp.zip")
+        shutil.copy(volume_path, ".tmp.zip")
         volume = re.search(r"v[0-9]{1,3}", volume_path[volume_path.rfind("/") + 1 :])
         volume = int(volume.group(0)[1:])
 
-        print(f"[EXTRACTING] {title}{volume_path[volume_path.rfind("/"):]}")
-        with ZipFile(f"{base_directory}/tmp.zip", "r") as zObject:
-            zObject.extractall(path=f"{base_directory}/{volume}")
+        print(f"[EXTRACTING] {TITLE}{volume_path[volume_path.rfind("/"):]}")
+        with ZipFile(f"{base_directory}/.tmp.zip", "r") as zObject:
+            zObject.extractall(path=f"{base_directory}/.{volume}")
 
-        os.remove("tmp.zip")
+        os.remove(".tmp.zip")
 
         # Note that volume_path in this case refers to the *original* volume path, not the extracted one. We have to use the extracted one
         # for the build.
-        build_chapters(base_directory, volume, f"{base_directory}/{volume}")
+        build_chapters(base_directory, volume, f"{base_directory}/.{volume}")
 
 
 if __name__ == "__main__":
